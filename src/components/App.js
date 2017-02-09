af87f5b4c1f55e220e0a48f4c77dbcee0eaed85f
Ryo Suzuki <ryosuzk@gmail.com>
2017-02-09 14:28:25 -0700

    Start implementing with Snap.svg

--- src/components/App.js
+++ src/components/App.js
@@ -5,21 +5,62 @@ class App extends Component {
     super(props)
     window.app = this
     this.state = {}
+    this.mousedown = false
   }
 
   componentDidMount() {
+    this.width = window.innerWidth
+    this.height = window.innerHeight
+    this.paper = Snap(this.width, this.height).remove();
+
+    this.paper.mousedown((e) => {
+      this.onMouseDown(e)
+    })
+    this.paper.mousemove((e) => {
+      this.onMouseMove(e)
+    })
+    this.paper.mouseup((e) => {
+      this.onMouseUp(e)
+    })
+    // var circle = paper.circle(-10,-10,10).attr("fill", "red");
+    // paper.mousemove(function(e){
+    //   var m = (Snap.matrix(paper.node.getScreenCTM())).invert();
+    //   var ex = e.clientX;
+    //   var ey = e.clientY;
+    //   circle.attr({cx:m.x(ex, ey), cy:m.y(ex, ey)});
+    // });
+
+    const container = document.querySelector("#container");
+    this.paper.prependTo(container);
+  }
+
+  onMouseDown(e) {
+    console.log('mouse down')
+    this.mousedown = true
+  }
+
+  onMouseMove(e) {
+    if (!this.mousedown) return false
+    console.log('mouse move')
+    console.log(this.getCursor(e))
+  }
+
+  onMouseUp(e) {
+    console.log('mouse up')
+    this.mousedown = false
+  }
+
+  getCursor(e) {
+    const m = (Snap.matrix(this.paper.node.getScreenCTM())).invert()
+    const ex = e.clientX
+    const ey = e.clientY
+    return { x: m.x(ex, ey), y: m.y(ex, ey) }
   }
 
   render() {
     return (
       <div>
-        <div className="ui column centered grid">
-          <div className="nine wide column">
-            <h1 id="top" className="ui center aligned huge header">
-              Exploring the Design Space of Automated Hints
-            </h1>
-          </div>
-        </div>
+        <div id="container"></div>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import 'semantic-ui-css/semantic.js'
 import 'rc-slider/dist/rc-slider.css'
 import 'rc-tooltip/assets/bootstrap.css'
 import './style.css'
+import 'imports-loader?this=>window,fix=>module.exports=0!snapsvg/dist/snap.svg.js';
+
 
 import React from 'react'
 import { render } from 'react-dom'
